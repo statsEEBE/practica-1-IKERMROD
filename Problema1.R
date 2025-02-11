a8bb0f42e620fd2b347d2bee1186ba0d9edeb643
iker.munoz.rodriguez <iker.munoz.rodriguez@auleseebe.upc.edu>
2025-02-11 09:16:12 +0100

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,35 @@
 #Codigo para problema 1
 
-plot(1:5)
+mis_dades <- mtcars
+mis_dades
+dim(mis_dades)
+
+mis_dades$qsec
+
+#promedio
+mean(mis_dades$qsec)
+
+#mediana (2º cuartil, Q0.5)
+sort(mis_dades$drat)
+median(mis_dades$drat)
+
+#rango intercuartilico, Q0.25 -> 1r cuartil, Q0.5 -> 2n cuartil, Q0.75 ->3r cuatril
+quantile(mis_dades$drat, 0.25)
+quantile(mis_dades$drat, 0.75)
+IQR(mis_dades$drat)
+
+boxplot(mis_dades$drat)
+
+quantile(mis_dades$mpg, 0.18)
+
+IQR(mis_dades$cyl)
+
+#desviacion tipica
+sd(mis_dades$cyl)
+
+#varianza muestral
+var(mis_dades$qsec)
+
+x<- mis_dades$qsec
+
+sum((x-mean(x))^2)/(length(x)-1)
